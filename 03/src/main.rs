015a33600f65cccf4162693b56a7b4c7c272e262
Andrea Ronco <andrea.ronco@pbl.ee.ethz.ch>
2023-12-03 19:09:01 +0100

    add: part 1 and 2 combined

--- 03/src/main.rs
+++ 03/src/main.rs
@@ -19,57 +19,87 @@ fn main() {
 
     if let Ok(mut lines) = read_lines(path) {
 
-        let line = match lines.next() {
-            Some(Ok(l)) => l,
-            _ => {
-                println!("Error parsing input file");
-                return;
-            },
-        };
-        let mut prev_symbols = find_symbols(&line);
-        let mut prev_numbers = find_numbers(&line);
+        let line_t2 = unpack_line(lines.next());
+        let line_t1 = unpack_line(lines.next());
 
-        let mut sum = 0;
+        let mut line_window = (line_t1, line_t2, String::new());
+
+        let mut sum_components = 0;
+        let mut sum_gear = 0;
 
         for line in lines {
             if let Ok(l) = line {
-                let symbols = find_symbols(&l);
-                let numbers = find_numbers(&l);
-                for sym_pos in symbols.iter() {
+                // shift lines
+                line_window.2 = line_window.1;
+                line_window.1 = line_window.0;
+                line_window.0 = l;
+
+                let symbols_t1 = find_symbols(&line_window.1);
+
+                let numbers_t2 = find_numbers(&line_window.2);
+                let mut numbers_t1 = find_numbers(&line_window.1);
+                let mut numbers_t0 = find_numbers(&line_window.0);
+
+                let mut numbers = numbers_t2;
+
+                numbers.append(&mut numbers_t1);
+                // find adjacent numbers
+                for sym_pos in symbols_t1.iter() {
                     for (num_start, num_end, num) in numbers.iter() {
-                        if is_adjacent(*sym_pos, (*num_start, *num_end)) {
-                            // println!("current: {}", num);
-                            sum += num;
-                        }
-                    }
-                    for (num_start, num_end, num) in prev_numbers.iter() {
-                        if is_adjacent(*sym_pos, (*num_start, *num_end)) {
-                            // println!("previous nums: {}", num);
-                            sum += num;
+                        if is_adjacent(sym_pos.0, (*num_start, *num_end)) {
+                            sum_components += num;
                         }
                     }
                 }
 
-                for sym_pos in prev_symbols.iter() {
-                    for (num_start, num_end, num) in numbers.iter() {
-                        if is_adjacent(*sym_pos, (*num_start, *num_end)) {
-                            // println!("previous symb: {}", num);
-                            sum += num;
+                // find adjacent symbols
+                for sym_pos in symbols_t1.iter() {
+                    for (num_start, num_end, num) in numbers_t0.iter() {
+                        if is_adjacent(sym_pos.0, (*num_start, *num_end)) {
+                            sum_components += num;
                         }
                     }
                 }
-                prev_symbols = symbols;
-                prev_numbers = numbers;
 
+                // squash all numbers
+                numbers.append(&mut numbers_t0);
+                for sym_pos in symbols_t1.iter() {
+                    if sym_pos.1 != '*' {
+                        continue;
+                    }
+                    let mut adj_cnt = 0;
+                    let mut sym_power = 1;
+                    for (num_start, num_end, num) in numbers.iter() {
+                        if is_adjacent(sym_pos.0, (*num_start, *num_end)) {
+                            adj_cnt += 1;
+                            sym_power *= num;
+                        }
+                    }
+                    if adj_cnt ==  2 {
+                        sum_gear += sym_power;
+                    }
+                }
             }
         }
-        println!("Sum: {}", sum);
+        println!("Sum Part Numbers: {}", sum_components);
+        println!("Sum Gear Ratios: {}", sum_gear);
     }
 }
 
-fn find_symbols(line: &str) -> Vec<usize>{
+
+fn unpack_line(line_option: Option<Result<String,io::Error>>) -> String {
+    match line_option {
+        Some(Ok(l)) => l,
+        _ => {
+            println!("Error parsing input file");
+            return String::new();
+        },
+    }
+}
+
+fn find_symbols(line: &str) -> Vec<(usize, char)>{
     let re = Regex::new(r"[^\w\s.]").unwrap();
-    re.find_iter(line).map(|m| m.start()).collect()
+    re.find_iter(line).map(|m| (m.start(), m.as_str().chars().next().unwrap())).collect()
 
 }
 
